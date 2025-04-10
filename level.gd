4de334bd16df92890556f0cfdf393f7d5aaff292
VladislavIvanov-study <vladivanov060514@gmail.com>
2025-04-10 17:50:43 +0300

    добавление сцен уровня 2 и 3, и функцию для переключения на них

--- level.gd
+++ level.gd
@@ -13,3 +13,10 @@ func _process(delta: float) -> void:
 
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_2.tscn")
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_3.tscn")
