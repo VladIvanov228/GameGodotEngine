a47034012f420322735a20d4be2aa40106573b1d
VladislavIvanov-study <vladivanov060514@gmail.com>
2025-04-15 19:33:57 +0300

    Добавление персонажа Скелет, добавление анимации для него, а также его скрипта. А именно, преследование игрока, смерть и простейшее нанесение урона.

--- PlayerRepository/player.gd
+++ PlayerRepository/player.gd
@@ -5,7 +5,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var anim = $AnimatedSprite2D #get_node("AnimatedSprite2D")
-
+var health = 100
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
